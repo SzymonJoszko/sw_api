e59a5e6fa5e4e96505a43eff49e111bf7ba1be75
SzymonJoszko <48184291+SzymonJoszko@users.noreply.github.com>
2023-05-09 17:59:11 +0200

    Prepare api (#2)
    * delete unused api methods
    	* fix problem with Person.all_objects

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,51 +1,14 @@
 class SpeciesController < ApplicationController
-  before_action :set_species, only: %i[ show update destroy ]
-
-  # GET /species
-  def index
-    @species = Specie.all
-
-    render json: @species
-  end
+  before_action :set_species, only: %i[ show ]
 
   # GET /species/1
   def show
     render json: @species
   end
 
-  # POST /species
-  def create
-    @species = Specie.new(species_params)
-
-    if @species.save
-      render json: @species, status: :created, location: @species
-    else
-      render json: @species.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /species/1
-  def update
-    if @species.update(species_params)
-      render json: @species
-    else
-      render json: @species.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /species/1
-  def destroy
-    @species.destroy
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_species
-      @species = Specie.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def species_params
-      params.fetch(:species, {})
+      @species = Specie.find_object(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :vehicles
-  resources :starships
-  resources :species
-  resources :planets
-  resources :people
-  resources :films
+  resources :vehicles, only: %i[show]
+  resources :starships, only: %i[show]
+  resources :species, only: %i[show]
+  resources :planets, only: %i[show]
+  resources :people, only: %i[show index]
+  resources :films, only: %i[show index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
